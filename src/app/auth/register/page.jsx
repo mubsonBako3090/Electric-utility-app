b44255ae00cfc9fbcd114061a19893b0d3b372e8
Your Name <your@email.com>
2025-11-11 14:50:07 +0100

    fix path

--- src/app/auth/register/page.jsx
+++ src/app/auth/register/page.jsx
@@ -1,6 +1,6 @@
 'use client';
 import { useState, useEffect } from 'react';
-import { useAuth } from '../../../../contexts/AuthContext';
+import { useAuth } from '@/contexts/AuthContext';
 import { useRouter } from 'next/navigation';
 import Header from '@/components/Navbar';
 import Footer from '@/components/Footer';
