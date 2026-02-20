6ed60bfb95012dc0f753774c569a585e2664d085
Adam Mubarak Bako <mubarakadambako@gmail.com>
2026-02-20 10:27:43 +0100

    Update EnergyReading.js

--- src/models/EnergyReading.js
+++ src/models/EnergyReading.js
@@ -1,10 +1,36 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-const EnergyReadingSchema = new mongoose.Schema({
-  feederId: { type: mongoose.Schema.Types.ObjectId, ref: "Feeder" },
-  date: Date,
-  hoursSupplied: Number,
+const energyReadingSchema = new mongoose.Schema({
+  customerId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Customer',
+    required: true,
+  },
+  readingValue: {
+    type: Number,
+    required: true,
+  },
+  readingDate: {
+    type: Date,
+    required: true,
+  },
+  readingType: {
+    type: String,
+    enum: ['manual', 'automated', 'estimated'],
+    default: 'manual',
+  },
+  meterImage: String,
+  verifiedBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  consumption: {
+    type: Number, // calculated consumption since last reading
+  },
 }, { timestamps: true });
 
-export default mongoose.models.EnergyReading ||
-  mongoose.model("EnergyReading", EnergyReadingSchema);
+// Index for efficient queries
+energyReadingSchema.index({ customerId: 1, readingDate: -1 });
+
+const EnergyReading = mongoose.models.EnergyReading || mongoose.model('EnergyReading', energyReadingSchema);
+export default EnergyReading;
