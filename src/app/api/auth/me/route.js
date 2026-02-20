fc5c6f615c71b76a68b5832018748a786507e66b
Adam Mubarak Bako <mubarakadambako@gmail.com>
2026-02-20 14:46:13 +0100

    Update route.js

--- src/app/api/auth/me/route.js
+++ src/app/api/auth/me/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
 import { getSession } from '@/lib/auth';
-import connectDB from '@/lib/mongodb';
+import { connectDB } from '@/lib/mongodb';
 import User from '@/models/User';
 
 export async function GET() {
